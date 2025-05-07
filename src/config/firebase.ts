92d0fcb724a288ce6b2fdf00a1d5b1dacf2e48c3
Sebastian Troć <SebastianTroc@users.noreply.github.com>
2025-05-08 00:40:14 +0200

    chore: clear Firebase config values

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJnPg0GP1qoZ7JoJSNUgvUca89yYdzFDM",
-  authDomain: "riley-brown-app.firebaseapp.com",
-  projectId: "riley-brown-app",
-  storageBucket: "riley-brown-app.appspot.com",
-  messagingSenderId: "978796645235",
-  appId: "1:978796645235:web:27ae4d886611cdd6bf1306",
-  measurementId: "G-M3P3GNF0ZB",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
